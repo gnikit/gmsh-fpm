e77b53a73966fc3788675de540ade018c4af9e9b
gnikit <giannis.nikiteas@gmail.com>
2022-09-05 13:40:39 +0100

    ci: move version script to .github

--- .github/version.py
+++ .github/version.py
@@ -9,14 +9,14 @@ version_api = ""
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 # Get the version from the API version
-with open(os.path.join(dir_path, "src", "gmsh.f90"), "r") as f:
+with open(os.path.join(dir_path, "..", "src", "gmsh.f90"), "r") as f:
     for line in f:
         if "public :: GMSH_API_VERSION =" in line:
             version_api = line.split("GMSH_API_VERSION =")[1].strip().strip('"')
             break
 
 # Get the version from the fpm.toml
-with fileinput.input(os.path.join(dir_path, "fpm.toml"), inplace=True) as f:
+with fileinput.input(os.path.join(dir_path, "..", "fpm.toml"), inplace=True) as f:
     for line in f:
         if line.startswith("version = "):
             version_fpm = line.split("=")[1].strip().strip('"')
