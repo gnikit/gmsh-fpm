9125b588a73c834c0495b489538f68edf8eb2bec
gnikit <giannis.nikiteas@gmail.com>
2022-09-05 14:36:44 +0100

    ci: uses a version file for fpm

--- .github/version.py
+++ .github/version.py
@@ -8,7 +8,9 @@ version_fpm = ""
 version_api = ""
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-# Get the version from the API version
+# Get the version from the API version, this version can be different from the
+# latest tag version, since Gmsh dev cycle tends to increment the version number
+# first and release the tag later once done.
 with open(os.path.join(dir_path, "..", "src", "gmsh.f90"), "r") as f:
     for line in f:
         if "public :: GMSH_API_VERSION =" in line:
@@ -16,18 +18,9 @@ with open(os.path.join(dir_path, "..", "src", "gmsh.f90"), "r") as f:
             break
 
 # Get the version from the fpm.toml
-with fileinput.input(os.path.join(dir_path, "..", "fpm.toml"), inplace=True) as f:
-    for line in f:
-        if line.startswith("version = "):
-            version_fpm = line.split("=")[1].strip().strip('"')
-            # If the version is out of date, update it
-            if version_fpm != version_api:
-                new_line = line.replace(version_fpm, version_api)
-                print(new_line, end="")
-            else:
-                print(line, end="")
-        else:
-            print(line, end="")
+with open(os.path.join(dir_path, "version.txt"), "r") as f:
+    version_fpm = f.readlines()[0]
+
 
 if version_fpm != version_api:
     print(version_api)
