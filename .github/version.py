8a269d42ac439297c6a4a2064a7fe8bf5d336679
gnikit <giannis.nikiteas@gmail.com>
2024-03-13 06:40:21 +0000

    refactor: add encoding kwargs

--- .github/version.py
+++ .github/version.py
@@ -8,21 +8,21 @@ dir_path = Path(__file__).resolve().parent
 
 
 # Get the version from the fpm.toml
-with open(dir_path.parent / "version.txt") as f:
+with open(dir_path.parent / "version.txt", encoding="utf-8") as f:
     version_fpm = f.readlines()[0]
 
 # Get the version from the latest release, we cannot use the version stated
 # in the API because the development cycle of Gmsh tends to first increment the
 # version internally and then create a tag, which would cause our releases
 # to contain the wrong API files for the corresponding tag.
-with open(dir_path / "latest_tag.txt") as f:
+with open(dir_path / "latest_tag.txt", encoding="utf-8") as f:
     version_tag = f.readlines()[0].strip()
     # The Gmsh tag format is "gmsh_major_minor_patch"
     version_tag = ".".join(version_tag.split("_")[1:])
 
 if version_fpm != version_tag:
     # udpate the version.txt file
-    with open(dir_path.parent / "version.txt", "w") as f:
+    with open(dir_path.parent / "version.txt", "w", encoding="utf-8") as f:
         f.write(version_tag)
     print(version_tag)
 else:
